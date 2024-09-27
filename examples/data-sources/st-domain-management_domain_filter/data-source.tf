a48cc265d87e2af07af9fd0d71fcf6887987def1
LanTenDragon <pantheonia1@gmail.com>
2024-09-27 11:36:22 +0800

    Update documentation

--- examples/data-sources/st-domain-management_domain_filter/data-source.tf
+++ examples/data-sources/st-domain-management_domain_filter/data-source.tf
@@ -1,7 +1,7 @@
 data "st-domain-management_domain_filter" "example" {
   domain_labels = jsonencode({
-    "common/brand" = "brand-A"
-    "common/status" = "new"
+    "common/brand"   = "brand-A"
+    "common/status"  = "new"
     "common/project" = "project-B"
   })
 }
